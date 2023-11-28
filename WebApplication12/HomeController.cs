b7dd035c241c57e44ef30d3372458c7690558a21
Lirirum <prime102102@gmile.com>
2023-11-28 12:01:25 +0200

    First

---
+++ WebApplication12/HomeController.cs
@@ -0,0 +1,59 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplication12.Models;
+
+namespace WebApplication12
+{
+ 
+ 
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View("./Views/Registration.cshtml");
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(RegistrationModel  registration)
+
+        {   
+            
+
+
+            if (registration.Date.Date < DateTime.Now.Date)
+            {
+                ModelState.AddModelError("Date","Дата некоректна");
+                
+            }
+
+            if (registration.Date.DayOfWeek == DayOfWeek.Saturday || registration.Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                ModelState.AddModelError("Date", "Консультація не може бути у вихідні дні");
+            }
+
+
+            if (registration.Date.DayOfWeek == DayOfWeek.Monday && registration.Product== "Основи")
+            {
+
+                ModelState.AddModelError("Date", "Консультація по основам не проходять по понеділкам");
+            }
+
+
+            if (ModelState.IsValid)
+                return RedirectToAction("Sucsess");
+
+            return View("./Views/Registration.cshtml",registration);
+
+        }
+
+        public IActionResult Sucsess()
+        {
+            return View("./Views/Sucsess.cshtml");
+        }
+
+
+    }
+}
